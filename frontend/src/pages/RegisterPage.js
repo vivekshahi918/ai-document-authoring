f8683e603b872634b56b8278923e9631bbd4248f
Vivek Shahi <shahivivek503@gmail.com>
2025-11-20 03:15:30 +0530

    Update URL

--- frontend/src/pages/RegisterPage.js
+++ frontend/src/pages/RegisterPage.js
@@ -2,7 +2,7 @@
 
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom'; // <-- IMPORT useNavigate
-import axios from 'axios';
+import api from '../services/api';
 import { toast } from 'react-toastify'; // <-- IMPORT toast
 
 import './AuthPage.css';
